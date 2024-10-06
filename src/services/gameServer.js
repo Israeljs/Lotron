3330c13afbd76958226722baabf9dab17ba00671
israel jeronimo <silvaij16@gmail.com>
2024-10-06 00:30:48 -0300

    refactore: gameService

--- src/services/gameServer.js
+++ src/services/gameServer.js
@@ -1,11 +1,16 @@
 const gameGenerator = require('../helper/gameGenerator')
+const sumOfTensFilter = require('../filters/basicFilters/sumOfTensFilter')
 
 module.exports = {
   gameFilter: (quantity) =>  {
     const games = []
+
     for (let i = 0; i < quantity; i++) {
       games.push(gameGenerator())
     }
-    return games
+
+    const gamesFiltered = games.filter((game) => sumOfTensFilter(game))
+
+    return gamesFiltered
   },
 }
