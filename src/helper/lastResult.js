2acb59d291b2a16aaac2c4bd41dc78cd5befdf02
israel jeronimo <silvaij16@gmail.com>
2024-10-06 20:29:27 -0300

    refactore: lastResult

--- src/helper/lastResult.js
+++ src/helper/lastResult.js
@@ -3,9 +3,17 @@ const resultsJson = JSON.parse(results)
 
 const lastResult = (quantity) => {
   let games = []
-  for (let i = 0; i < quantity; i++) {
-    games.push(`concurso: ${resultsJson[i].concurso}`, resultsJson[i].dezenas.map(Number))
+
+  if (quantity > 0) {
+    for (let i = 0; i < quantity; i++) {
+      games.push(`concurso: ${resultsJson[i].concurso}`, resultsJson[i].dezenas.map(Number))
+    }
+  } else {
+    const allResults = resultsJson.map((resultJson)=> resultJson.dezenas.map(Number))
+    
+    return allResults
   }
+  
   return games
 }
 
