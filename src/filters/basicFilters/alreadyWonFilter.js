404c8a774f7489aeb1a1bd1bcf4979dcd03cc4b3
israel jeronimo <silvaij16@gmail.com>
2024-10-06 21:05:00 -0300

    feature: alreadyWonFilter

---
+++ src/filters/basicFilters/alreadyWonFilter.js
@@ -0,0 +1,22 @@
+const lastResult = require('../../helper/lastResult')
+
+const isEqual = (result, game) => {
+  let count = 0
+  let isTrue = false
+
+  result.forEach((ten) => {
+    if (game.includes(ten)) count += 1
+  })
+
+  if (count < 14) isTrue = true
+
+  return isTrue
+}
+
+const alreadyWonFilter = (game) => {
+  allResults = lastResult()
+
+  return  allResults.every(({ result }) => isEqual(result.map(Number), game))
+}
+
+module.exports = alreadyWonFilter
