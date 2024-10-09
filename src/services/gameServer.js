0fcbb82ce48fbbae5d5c76c6c820915357ee27aa
israel jeronimo <silvaij16@gmail.com>
2024-10-08 21:51:42 -0300

    implementação do filtro previousTensfilter.js

--- src/services/gameServer.js
+++ src/services/gameServer.js
@@ -1,6 +1,7 @@
 const gameGenerator = require('../helper/gameGenerator')
 const sumOfTensFilter = require('../filters/basicFilters/sumOfTensFilter')
 const alreadyWonFilter = require('../filters/basicFilters/alreadyWonFilter')
+const previousTensFilter = require('../filters/basicFilters/previousTensFilter')
 
 module.exports = {
   gameFilter: (quantity) =>  {
@@ -12,6 +13,7 @@ module.exports = {
 
     const gamesFiltered = games.filter((game) => sumOfTensFilter(game)[0])
       .filter((game) => alreadyWonFilter(game)[0])
+      .filter((game) => previousTensFilter(game)[0])
 
     return gamesFiltered
   },
