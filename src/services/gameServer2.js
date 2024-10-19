3c595519a6521237e3dc51d383c658eb6e8c70b5
israel jeronimo <silvaij16@gmail.com>
2024-10-18 21:07:13 -0300

    feature: oddFilter.js

---
+++ src/services/gameServer2.js
@@ -0,0 +1,25 @@
+const gameGenerator = require('../helper/gameGenerator')
+const sumOfTensFilter = require('../filters/basicFilters/sumOfTensFilter')
+const alreadyWonFilter = require('../filters/basicFilters/alreadyWonFilter')
+const previousTensFilter = require('../filters/basicFilters/previousTensFilter')
+const fibonacciFilter = require('../filters/basicFilters/fibonacciFilter')
+const oddFilter = require('../filters/basicFilters/oddFilter')
+
+const gameFilter = (quantity) => {
+  const games = []
+
+  for (let i = 0; i < quantity; i++) {
+    games.push(gameGenerator())
+  }
+
+  const gamesFiltered = games.filter((game) => sumOfTensFilter(game)[0])
+    .filter((game) => alreadyWonFilter(game))
+    .filter((game) => previousTensFilter(game)[0])
+    .filter((game) => fibonacciFilter(game)[0])
+    .filter((game) => oddFilter(game)[0])
+  console.log(gamesFiltered.length)
+  return gamesFiltered
+}
+console.log(gameFilter(50))
+
+module.exports = gameFilter
