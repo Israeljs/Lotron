6ee2f5fd5c2f57fd2b8ce651a91d095be7feb197
israel jeronimo <silvaij16@gmail.com>
2024-10-25 20:38:07 -0300

    feature: gameServer2.js

--- src/services/gameServer2.js
+++ src/services/gameServer2.js
@@ -1,25 +1,38 @@
-const gameGenerator = require('../helper/gameGenerator')
+const lastResult = require('../helper/lastResult')
+// const gameGenerator = require('../helper/gameGenerator')
 const sumOfTensFilter = require('../filters/basicFilters/sumOfTensFilter')
-const alreadyWonFilter = require('../filters/basicFilters/alreadyWonFilter')
+// const alreadyWonFilter = require('../filters/basicFilters/alreadyWonFilter')
 const previousTensFilter = require('../filters/basicFilters/previousTensFilter')
 const fibonacciFilter = require('../filters/basicFilters/fibonacciFilter')
 const oddFilter = require('../filters/basicFilters/oddFilter')
 
-const gameFilter = (quantity) => {
-  const games = []
+const percentage = (numerator ,denominator) => {
+// return `${(numerator/denominator)*100}%`
+return (numerator/denominator)*100
+}
 
-  for (let i = 0; i < quantity; i++) {
-    games.push(gameGenerator())
-  }
+const games = lastResult()
 
-  const gamesFiltered = games.filter((game) => sumOfTensFilter(game)[0])
-    .filter((game) => alreadyWonFilter(game))
+
+const gameFilter = () => {
+  console.log(games.length) //3189
+
+  // const gamesFiltered = games.filter((game) => sumOfTensFilter(game)[0]) // 95%
+  // const gamesFiltered = games.filter((game) => previousTensFilter(game)[0]) // 77%
+  // const gamesFiltered = games.filter((game) => fibonacciFilter(game)[0]) // 98%
+  // const gamesFiltered = games.filter((game) => oddFilter(game)[0]) // 95%
+  const gamesFiltered = games.filter((game) => sumOfTensFilter(game)[0]) // 95%
+    // .filter((game) => alreadyWonFilter(game))
     .filter((game) => previousTensFilter(game)[0])
     .filter((game) => fibonacciFilter(game)[0])
     .filter((game) => oddFilter(game)[0])
+
   console.log(gamesFiltered.length)
-  return gamesFiltered
+  console.log(`${Math.floor(percentage(gamesFiltered.length, games.length))}%`) // (`${Math.floor(percentage(2234, 3189))}%`)
+
+  // return gamesFiltered
+  return true
 }
-console.log(gameFilter(50))
+ gameFilter()
 
 module.exports = gameFilter
