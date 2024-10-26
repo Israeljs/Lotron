64589ce775e3cabc0746239c27336af04a5420ca
israel jeronimo <silvaij16@gmail.com>
2024-10-26 19:18:57 -0300

    feature: frameFilter.js

--- src/services/gameServer.js
+++ src/services/gameServer.js
@@ -4,6 +4,7 @@ const alreadyWonFilter = require('../filters/basicFilters/alreadyWonFilter')
 const previousTensFilter = require('../filters/basicFilters/previousTensFilter')
 const fibonacciFilter = require('../filters/basicFilters/fibonacciFilter')
 const oddFilter = require('../filters/basicFilters/oddFilter')
+const frameFilter = require('../filters/basicFilters/frameFilter')
 
 const gameFilter = (quantity) => {
   const games = []
@@ -17,11 +18,13 @@ const gameFilter = (quantity) => {
     .filter((game) => previousTensFilter(game)[0])
     .filter((game) => fibonacciFilter(game)[0])
     .filter((game) => oddFilter(game)[0])
+    .filter((game) => frameFilter(game)[0])
 
+  console.log(games.length)
   console.log(gamesFiltered.length)
 
   return gamesFiltered
 }
-console.log(gameFilter())
+console.log(gameFilter(5000))
 
 module.exports = gameFilter
